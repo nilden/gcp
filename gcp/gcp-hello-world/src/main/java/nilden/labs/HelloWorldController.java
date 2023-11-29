f55d83d0c3fa0fcf7c558af94eba04e4181874c5
nilden <stefan.nilden@mail.com>
2023-11-29 17:18:58 +0100

    Adding second commit

--- gcp/gcp-hello-world/src/main/java/nilden/labs/HelloWorldController.java
+++ gcp/gcp-hello-world/src/main/java/nilden/labs/HelloWorldController.java
@@ -1,4 +1,4 @@
-package nilden.labs.gcphelloworld;
+package nilden.labs;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
